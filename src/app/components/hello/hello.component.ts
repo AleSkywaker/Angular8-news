827f46df11160d0e46d073bbb19f291c01416512
Alex Colombo <uricaine@hotmail.com>
2019-12-21 21:20:24 +0100

    new component

--- src/app/components/hello/hello.component.ts
+++ src/app/components/hello/hello.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-custom-hello",
+  selector: 'app-custom-hello',
   template: `
     <p>Hello world!</p>
   `
 })
 export class HelloComponent {
-  titulo = "Hola";
+  titulo = 'Hola';
 }
