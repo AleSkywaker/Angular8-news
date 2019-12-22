fd807d02a1700a2c7807e47a381af9bd7ca45557
Alex Colombo <uricaine@hotmail.com>
2019-12-22 14:25:03 +0100

    wip on custom component

--- src/app/components/hello/hello.component.ts
+++ src/app/components/hello/hello.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-custom-hello",
-  templateUrl: "./hello.component.html"
+  templateUrl: "./hello.component.html",
+  styleUrls: ["./hello.component.css"]
 })
 export class HelloComponent {
   titulo = "Hola Alex";
